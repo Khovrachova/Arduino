1e89cda9a715018fc300e5aec86f942899be6277
Khovrachova <filippovapolina92@yandex.ru>
2021-04-03 00:48:46 +0300

    add segment

---
+++ segment.ino
@@ -0,0 +1,148 @@
+int up_right=
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int down_left=5;
+int point=8;
+int up_left=9;
+int t=500;
+int buzzer=10;
+
+void setup() {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      //7777
+pinModt,OUTPUTTPUT);
+pinMode(buzzer,OUTPUT);
+delay(t);
+}
+void NONE(){
+digitalWrite(down_right,LOW);
+digitalWrite(up_right,LOW);
+digitalWrite(up,LOW);
+digitalWrite(center,LOW);
+digitalWrite(down_left,LOW);
+digitalWrite(point,LOW);
+digitalWrite(up_left,LOW);
+digitalWrite(down,LOW);
+delay(t);
+}  
+void Buzzer(){
+digitalWrite(buzzer,HIGH);
+delay(t);
+digitalWrite(buzzer,LOW);
+delay(t);
+}
+void one(){
+digitalWrite(up_right,HIGH);
+digitalWrite(down_right,HIGH);
+delay(t);
+}
+void two(){
+digitalWrite(up,HIGH);
+digitalWrite(up_right,HIGH);
+digitalWrite(center,HIGH);
+digitalWrite(down_left,HIGH);
+digitalWrite(down,HIGH);
+delay(t);
+}
+
+void tree(){
+digitalWrite(up,HIGH);
+digitalWrite(up_right,HIGH);
+digitalWrite(center,HIGH);
+digitalWrite(down_right,HIGH);
+digitalWrite(down,HIGH);
+delay(t);
+}
+
+void four(){
+digitalWrite(center,HIGH);
+digitalWrite(up_right,HIGH);
+digitalWrite(up_left,HIGH);
+digitalWrite(down_right,HIGH);
+delay(t);
+}
+
+void five(){
+digitalWrite(up,HIGH);
+digitalWrite(up_left,HIGH);
+digitalWrite(center,HIGH);
+digitalWrite(down_right,HIGH);
+digitalWrite(down,HIGH);
+delay(t);
+}
+
+void seven(){
+digitalWrite(up_right,HIGH);
+digitalWrite(down_right,HIGH);
+digitalWrite(up,HIGH);
+delay(t);
+}
+void six(){
+digitalWrite(up,HIGH);
+digitalWrite(up_left,HIGH);
+digitalWrite(center,HIGH);
+digitalWrite(down_right,HIGH);
+digitalWrite(down,HIGH);
+digitalWrite(down_left,HIGH);
+digitalWrite(point,HIGH);
+delay(t);
+}
+
+void eight(){
+digitalWrite(down_right,HIGH);
+digitalWrite(up_right,HIGH);
+digitalWrite(up,HIGH);
+digitalWrite(center,HIGH);
+digitalWrite(down_left,HIGH);
+digitalWrite(up_left,HIGH);
+digitalWrite(down,HIGH);
+delay(t);
+}
+
+void nine(){
+digitalWrite(down_right,HIGH);
+digitalWrite(up_right,HIGH);
+digitalWrite(up,HIGH);
+digitalWrite(center,HIGH);
+digitalWrite(up_left,HIGH);
+digitalWrite(down,HIGH);
+digitalWrite(point,HIGH);
+delay(t);
+}
+void zero(){
+digitalWrite(down_right,HIGH);
+digitalWrite(up_right,HIGH);
+digitalWrite(up,HIGH);
+digitalWrite(down_left,HIGH);
+digitalWrite(up_left,HIGH);
+digitalWrite(down,HIGH);
+delay(t);
+}
+void loop(){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+digitalWrite(down_right, HIGH);
+}
