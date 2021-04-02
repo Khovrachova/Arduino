acba50ee4ee112ddecf6b15feaf1a2c454388768
Khovrachova <filippovapolina92@yandex.ru>
2021-04-03 00:33:31 +0300

    Add pult.ino

---
+++ pult.ino
@@ -0,0 +1,34 @@
+#include <IRremote.h> // подключаем библиотеку для IR приемника
+
+IRrecv irrecv(A0); // указываем пин, к которому подключен IR приемник
+decode_results results;
+
+void setup() {
+   irrecv.enableIRIn();  // запускаем прием инфракрасного сигнала
+   Serial.begin(9600); // подключаем монитор порта
+
+   pinMode(11, OUTPUT); // пин 11 будет выходом (англ. «output»)
+   pinMode(12, OUTPUT); // пин 12 будет выходом (англ. «output»)
+   pinMode(A0, INPUT); // пин A0 будет входом (англ. «intput»)
+
+}
+
+void loop() {
+   if (irrecv.decode(&results)) // если данные пришли выполняем команды
+   {
+      Serial.println(results.value); // отправляем полученные данные на порт
+    
+      // включаем и выключаем светодиод, в зависимости от полученного сигнала  
+      if (results.value == 16724175) { 
+      digitalWrite(12, HIGH);
+   }
+ // включаем и выключаем светодиод, в зависимости от полученного сигнала  
+      if (results.value ==16718055 ) { 
+      digitalWrite(11, HIGH);
+   }
+      if (results.value ==16753245 ) { 
+      digitalWrite(12, LOW);
+      digitalWrite(11, LOW);
+   }
+      irrecv.resume(); // принимаем следующий сигнал на ИК приемнике
+   }      }
